829b6203420f4183f42bd704a73db4457cf6872b
Catherine Mei <inspirit3318@gmail.com>
2019-07-25 10:27:15 -0500

    Can add search results to your chats page

--- main.py
+++ main.py
@@ -23,14 +23,213 @@ JINJA_ENV = jinja2.Environment(
 def root_parent():
     return ndb.Key('Parent', 'default_parent')
 
-def translateSentence(textToTranslate, sourceLang, targetLang):
+def translateSentence():
     try:
         form_data = urllib.urlencode({
-            'q': textToTranslate,
-            'source': sourceLang,
-            'target': targetLang,
+            'q': 'The Great Pyramid of Giza (also known as the Pyramid of Khufu or the Pyramid of Cheops) is the oldest and largest of the three pyramids in the Giza pyramid complex.',
+            'source': 'en',
+            'target': 'es',
             'format': 'text'
         })
+
+        languages = {
+        'aa': 'Afar',
+        'ab': 'Abkhazian',
+        'af': 'Afrikaans',
+        'ak': 'Akan',
+        'sq': 'Albanian',
+        'am': 'Amharic',
+        'ar': 'Arabic',
+        'an': 'Aragonese',
+        'hy': 'Armenian',
+        'as': 'Assamese',
+        'av': 'Avaric',
+        'ae': 'Avestan',
+        'ay':'Aymara',
+        'az': 'Azerbaijani',
+        'ba': 'Bashkir',
+        'bm': 'Bambara',
+        'eu': 'Basque',
+        'be': 'Belarusian',
+        'bn': 'Bengali',
+        'bi': 'Bislama',
+        'bo':'Tibetan',
+        'bs': 'Bosnian',
+        'br': 'Breton',
+        'bg': 'Bulgarian',
+        'my': 'Burmese',
+        'cs':'Czech',
+        'ch': 'Chamorro',
+        'ce': 'Chechen',
+        'zh': 'Chinese',
+        'cv': 'Chuvash',
+        'kw': 'Cornish',
+        'co': 'Corsican',
+        'cr': 'Cree',
+        'cy': 'Welsh',
+        'cs': 'Czech',
+        'da': 'Danish',
+        'de': 'German',
+        'dv': 'Divehi',
+        'dz': 'Dzongkha',
+        'el': 'Greek',
+        'en': 'English',
+        'eo': 'Esperanto',
+        'et': 'Estonian',
+        'eu': 'Basque',
+        'ee': 'Ewe',
+        'fo': 'Faroese',
+        'fa': 'Persian',
+        'fj': 'Fijian',
+        'fi':'Finnish',
+        'fr': 'French',
+        'fr': 'French',
+        'fy': 'Western Frisian',
+        'ff': 'Fulah',
+        'Ga': 'Georgian',
+        'de': 'German',
+        'gd': 'Gaelic',
+        'ga': 'Irish',
+        'gl': 'Galician',
+        'gv': 'Manx',
+        'gn': 'Guarani',
+        'gu': 'Gujarati',
+        'ht': 'Haitian; Haitian Creole',
+        'ha': 'Hausa',
+        'he': 'Hebrew',
+        'hz': 'Herero',
+        'hi': 'Hindi',
+        'ho': 'Hiri Motu',
+        'hr': 'Croatian',
+        'hu': 'Hungarian',
+        'hy': 'Armenian',
+        'ig': 'Igbo',
+        'is': 'Icelandic',
+        'io': 'Ido',
+        'ii': 'Sichuan Yi; Nuosu',
+        'iu': 'Inuktitut',
+        'ie': 'Interlingue; Occidental',
+        'ia': 'Interlingua (International Auxiliary Language Association)',
+        'id': 'Indonesian',
+        'ik': 'Inupiaq',
+        'is': 'Icelandic',
+        'it': 'Italian',
+        'jv': 'Javanese',
+        'ja': 'Japanese',
+        'kl': 'Kalaallisut; Greenlandic',
+        'kn': 'Kannada',
+        'ks': 'Kashmiri',
+        'ka': 'Georgian',
+        'kr': 'Kanuri',
+        'kk': 'Kazakh',
+        'km': 'Central Khmer',
+        'ki': 'Kikuyu; Gikuyu',
+        'rw': 'Kinyarwanda',
+        'ky': 'Kirghiz; Kyrgyz',
+        'kv': 'Komi',
+        'kg': 'Kongo',
+        'ko': 'Korean',
+        'kj': 'Kuanyama; Kwanyama',
+        'ku': 'Kurdish',
+        'lo': 'Lao',
+        'la': 'Latin',
+        'lv': 'Latvian',
+        'li': 'Limburgan; Limburger; Limburgish',
+        'ln': 'Lingala',
+        'lt': 'Lithuanian',
+        'lb': 'Luxembourgish; Letzeburgesch',
+        'lu': 'Luba-Katanga',
+        'lg': 'Ganda',
+        'mk': 'Macedonian',
+        'mh': 'Marshallese',
+        'ml': 'Malayalam',
+        'mi': 'Maori',
+        'mr': 'Marathi',
+        'ms': 'Malay',
+        'Mi': 'Micmac',
+        'mk': 'Macedonian',
+        'mg': 'Malagasy',
+        'mt': 'Maltese',
+        'mn': 'Mongolian',
+        'mi': 'Maori',
+        'ms': 'Malay',
+        'my': 'Burmese',
+        'na': 'Nauru',
+        'nv': 'Navajo; Navaho',
+        'nr': 'Ndebele, South; South Ndebele',
+        'nd': 'Ndebele, North; North Ndebele',
+        'ng': 'Ndonga',
+        'ne': 'Nepali',
+        'nl': 'Dutch; Flemish',
+        'no': 'Norwegian',
+        'oc': 'Occitan (post 1500)',
+        'oj': 'Ojibwa',
+        'or': 'Oriya',
+        'om': 'Oromo',
+        'os': 'Ossetian; Ossetic',
+        'pa': 'Punjabi',
+        'fa': 'Persian',
+        'pi': 'Pali',
+        'pl': 'Polish',
+        'pt': 'Portuguese',
+        'ps': 'Pushto; Pashto',
+        'qu': 'Quechua',
+        'rm': 'Romansh',
+        'ro': 'Romanian; Moldavian; Moldovan',
+        'rn': 'Rundi',
+        'ru': 'Russian',
+        'sg': 'Sango',
+        'sa': 'Sanskrit',
+        'si': 'Sinhala; Sinhalese',
+        'sk': 'Slovak',
+        'sl': 'Slovenian',
+        'se': 'Northern Sami',
+        'sm': 'Samoan',
+        'sn': 'Shona',
+        'sd': 'Sindhi',
+        'so': 'Somali',
+        'st': 'Sotho',
+        'es': 'Spanish',
+        'sq': 'Albanian',
+        'sc': 'Sardinian',
+        'sr': 'Serbian',
+        'ss': 'Swati',
+        'su': 'Sundanese',
+        'sw': 'Swahili',
+        'sv': 'Swedish',
+        'ty': 'Tahitian',
+        'ta': 'Tamil',
+        'tt': 'Tatar',
+        'te': 'Telugu',
+        'tg': 'Tajik',
+        'tl': 'Tagalog',
+        'th': 'Thai',
+        'bo': 'Tibetan',
+        'ti': 'Tigrinya',
+        'to': 'Tonga (Tonga Islands)',
+        'tn': 'Tswana',
+        'ts': 'Tsonga',
+        'tk': 'Turkmen',
+        'tr': 'Turkish',
+        'tw': 'Twi',
+        'ug': 'Uighur; Uyghur',
+        'uk': 'Ukrainian',
+        'ur': 'Urdu',
+        'uz': 'Uzbek',
+        've': 'Venda',
+        'vi': 'Vietnamese',
+        'cy': 'Welsh',
+        'wa': 'Walloon',
+        'wo': 'Wolof',
+        'xh': 'Xhosa',
+        'yi': 'Yiddish',
+        'yo': 'Yoruba',
+        'za': 'Zhuang; Chuang',
+        'zh': 'Chinese',
+        'zu': 'Zulu'
+        }
+
+
 
         headers = {'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization': 'Bearer ' + api_key.gcpkey
@@ -49,7 +248,6 @@ def toDict(msg):
     'sentFrom': msg.sentFrom,
     'sentTo': msg.sentTo,
     'msg': msg.msg,
-    'translated': msg.translated,
     'timeSent': msg.timeSent
     }
 
@@ -74,7 +272,6 @@ class Message(ndb.Model):
     sentTo = ndb.StringProperty()
     msg = ndb.StringProperty()
     timeSent = ndb.StringProperty()
-    translated = ndb.StringProperty()
 
 # the handler section
 class MainPage(webapp2.RequestHandler):
@@ -116,15 +313,12 @@ class ChatPage(webapp2.RequestHandler):
 
         def post(self):
             user = users.get_current_user()
-            my_user = User.query(User.id == user.user_id()).fetch()[0]
-            other_user = User.query(User.id == str(self.request.get("id"))).fetch()[0]
-
+            logging.debug(user.user_id())
             new_msg = Message(parent=root_parent())
             new_msg.sentFrom = user.user_id()
             new_msg.sentTo = str(self.request.get("id"))
             new_msg.msg = self.request.get("chatText")
             new_msg.timeSent = str(datetime.datetime.now())
-            new_msg.translated = translate(self.request.get("chatText"), my_user.languages_spoken, other_user.languages_spoken)
             new_msg.put()
             # redirect to '/' so that the get() version of this handler will run
             # and show the list of dogs.
@@ -410,12 +604,12 @@ class IntermediatePage(webapp2.RequestHandler):
         user = users.get_current_user()
         self.response.headers['Content-Type'] = 'text/html'
         index_template = JINJA_ENV.get_template('templates/intermediate.html')
-        me = User.query(User.id == user.user_id()).fetch()
+
+        me = User.query(User.id == user.user_id()).fetch()[0]
         listoffriends = []
-        if len(listoffriends) >0:
+        if len(me.friends) >0:
             for x in me.friends:
                 listoffriends.append(User.query(User.id == x).fetch()[0])
-
         values ={
         'user': user,
         'logout_url': users.create_logout_url('/'),
